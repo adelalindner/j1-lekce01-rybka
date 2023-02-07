07861afc500331e7e15c4d0fb143bc5229c90cf7
AdelaLindner <a.lindnerova@seznam.cz>
2023-02-07 20:06:36 +0100

    Rybka plave vpravo.

--- src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
+++ src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
@@ -30,6 +30,8 @@ public class RybkaApplication extends CzechitasGameApplication {
     protected void onStart() {
         kamen = new Kamen(200, 200);
         rybka = new Rybka(100, 100);
+
+        rybka.plavatVpravo();
     }
 
     public static void main(String[] args) {
